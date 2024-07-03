dc1a483d1131fa752875f43e64897e9d239d2323
EnlonW <github@enlon.me>
2024-07-03 09:53:37 +0800

    feat: vue-query use

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './app'
 import router from './app/routes'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
+
+app.use(VueQueryPlugin)
 
 app.use(createPinia())
 app.use(router)
